ae0bccaee091e13e2815a0dedf0175bfacf02f70
Gabrielboxe <gabrielnunesvet@gmail.com>
2025-06-10 20:50:17 -0300

    First commit

--- pacientes-api/src/main/java/br/com/exemplo/pacientes_api/repository/PacienteRepository.java
+++ pacientes-api/src/main/java/br/com/exemplo/pacientes_api/repository/PacienteRepository.java
@@ -1,6 +1,6 @@
 package br.com.exemplo.pacientes_api.repository;
 
-import br.com.exemplo.pacientesapi.model.Paciente;
+import br.com.exemplo.pacientes_api.model.Paciente;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
